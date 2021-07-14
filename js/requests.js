2b0179a59c9977aa0a46f01afe6c97be37e61cc4
Phil <philjacks.1987@gmail.com>
2021-07-14 23:13:23 +0100

    unnecessary code removed

--- js/requests.js
+++ js/requests.js
@@ -10,29 +10,3 @@ const getPuzzle = async wordCount => {
     throw new Error(`Unable to get puzzle`);
   }
 };
-
-const getCountry = async countryCode => {
-  const response = await fetch("https://restcountries.eu/rest/v2/all");
-
-  if (response.status === 200) {
-    const data = await response.json();
-    return data.find(country => country.alpha2Code === countryCode);
-  } else {
-    throw new Error("Unable to get country");
-  }
-};
-
-const getLocation = async () => {
-  const response = await fetch("https://ipinfo.io/json?token=fa5c2ae285502f");
-
-  if (response.status === 200) {
-    return response.json();
-  } else {
-    throw new Error(`Sorry, unable to get location`);
-  }
-};
-
-const getCurrentCountry = async () => {
-  const location = await getLocation();
-  return getCountry(location.country);
-};
